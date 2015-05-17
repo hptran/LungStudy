48041c46bd045b87c1685c390de5471209b7dd94
huy <user@users-Mac-Pro.local>
2015-05-16 19:58:06 -0700

    initial setup (add researchkit library, and simple task to test)

--- LungStudy/ViewController.swift
+++ LungStudy/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import ResearchKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ORKTaskViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        //create a step
+        let myStep = ORKInstructionStep(identifier: "intro")
+        myStep.title = "Welcome to ResearchKit";
+        //create a task
+        let task = ORKOrderedTask(identifier: "task", steps: [myStep])
+        //present the task
+        let taskViewController = ORKTaskViewController(task: task, taskRunUUID: nil)
+        taskViewController.delegate = self
+        presentViewController(taskViewController, animated: true, completion: nil)
+    }
+    
+    func taskViewController(taskViewController: ORKTaskViewController,
+        didFinishWithReason reason: ORKTaskViewControllerFinishReason,
+        error: NSError?) {
+            let taskResult = taskViewController.result
+            // You could do something with the result here.
+            
+            // Then, dismiss the task view controller.
+            dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
 
